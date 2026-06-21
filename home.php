4493ded2a6ecd67347b64db3260ad5b71d116f06
Arthur Torres <166349313+ArthurGuimaraesTorres@users.noreply.github.com>
2026-06-21 17:50:06 -0300

    Initial commit

---
+++ home.php
@@ -0,0 +1,10 @@
+<?php require __DIR__ . '/header.php'; ?>
+
+<div class="container mt-4 d-flex flex-column align-items-center">
+    <h1>Bem-vindo ao GTs. | Support</h1>
+    <p>Este é o sistema de suporte para a empresa GTs. Aqui você pode criar tickets de suporte, acompanhar o status dos seus chamados e entrar em contato com nossa equipe de atendimento.</p>
+
+    <button class="btn btn-primary">Criar Ticket</button>
+</div>
+
+<?php require __DIR__ . '/footer.php'; ?>
